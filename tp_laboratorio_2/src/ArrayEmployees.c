ffb516c93dc17845b9248b0652c155670a6fea66
alumno <alumno@LAB00PC00>
2019-10-03 22:16:36 -0300

    terminado

--- tp_laboratorio_2/src/ArrayEmployees.c
+++ tp_laboratorio_2/src/ArrayEmployees.c
@@ -32,7 +32,14 @@ int initEmployees(Employee *list, int len) {
 	}
 	return retorno;
 }
-
+/* \brief To indicate FREE  position in the array are empty,
+ * this function put the flag (isEmpty) in TRUE in all
+ * position of the array
+ * \param list Employee* Pointer to array of employees
+ * \param len int Array length
+ * \return int Return (-1) if Error [Invalid length or NULL pointer] - (0) if Ok
+ *
+ */
 int findPlaceFreeEmployee(Employee *list, int len) {
 	int retorno = -1;
 	int i;
